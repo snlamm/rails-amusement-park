a5d5b0e3668bd4750abbc604016d33804ffc7cd4
snlamm <shmuel.lamm@gmail.com>
2016-05-09 16:22:44 -0400

    add migrations. pre-migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
 
+  resources :rides
+
+  resources :users
+
+  resources :attractions
+
 end
---
+++ db/migrate/20160509201927_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :nausea
+      t.integer :happiness
+      t.integer :tickets
+      t.integer :height
+
+      t.timestamps null: false
+    end
+  end
+end
